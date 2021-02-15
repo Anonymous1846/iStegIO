3a349d07f71c000ab8877be8a8652e7ae2b16b42
Sharath Sunil <alphatrinity17@gmail.com>
2021-02-15 10:16:36 +0530

    Four major methods implemented

--- iStegIO.py
+++ iStegIO.py
@@ -5,10 +5,34 @@ the blue bits, with the text bits.
 '''
 
 import binascii
-from PIL import Image
-
+from PIL import Image,ImageColor
+'''
+The below function will take three int arguements and gives hex code for the corresponding color !
+params:r,g,b color code (int)
+return: hex code for color
+'''
 def rgb_to_hex(r,g,b):
 	return '#{:02x}{:02x}{:02x}'.format(r,g,b)
-
+'''
+The below function will take a hex code and gives a tuple containing the rgb values(three len)
+params:hex code for color !
+return: rgb tuple
+'''
 def hex_to_rgb(hexcode):
-	r,g,b=
+	#using the getcolor method we got the rgb tuple !
+	return ImageColor.getcolor(hexcode,"RGB")
+def string_to_binary(string):
+	#the output will be 0b01100100010010
+	#we have to translate the binary to 01100100010010 
+	binary=bin(int(binascii.hexlify(string.encode()),16)).replace('0b','')
+	return binary
+def binary_to_string(binary):
+	# we first convert the binary to hex and then into string !
+	#each hex bit is translated into int base 2 and then to string
+	#we get the byte stream of the message !
+	return binascii.unhexlify('%x' % int('0b'+binary,2))
+
+print(hex_to_rgb('#0e0f99'))
+print(rgb_to_hex(12,15,12))
+print(string_to_binary('Hello'))
+print(binary_to_string(string_to_binary('Hello')))
