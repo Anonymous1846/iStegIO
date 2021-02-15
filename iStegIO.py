f40cb00289d0bc149035a87474c132ead1a0b22f
Sharath Sunil <alphatrinity17@gmail.com>
2021-02-15 12:24:01 +0530

    Extract Function to be added !

--- iStegIO.py
+++ iStegIO.py
@@ -41,8 +41,52 @@ def binary_to_string(binary):
 	#each hex bit is translated into int base 2 and then to string
 	#we get the byte stream of the message !
 	return binascii.unhexlify('%x' % int('0b'+binary,2))
-
-def hide(filename,message):
-	pass
+'''
+If end of the hex code is ether 0 or 1, then return the last part of the hexcode
+params: hexcode
+return: last hex
+'''
+def decrypt(hexcode):
+	if hexcode[-1] in [0,1]:
+		return hexcode[-1]
+	else:return None
+def encrypt(hexcode,digit):
+	if hexcode[-1] in ['0','1','2','3','4','5']:
+		hexcode=hexcode[:-1]+digit
+		return hexcode
+	else:return None
+'''
+We'll use all the helper function above to hide the message string within the photo
+'''
+def hide(filename,message,out):
+	image =Image.open(filename)
+	#convert the message to binary and append 15 ones and one zero !
+	binary=string_to_binary(message)+'1111111111111110'
+	if image.mode in ('RGBA'):
+		image =image.convert('RGBA')
+		#getting the data from the image !
+		img_data=image.getdata()
+		#the text and the image will appended here !
+		enc_data=[]
+		#the current bin digit we are working on !
+		digit = 0
+		for i in img_data:
+			if (digit < len(binary)):
+				#reference made to the rgb_to_hex function to convert the rgb tuple to the hexadecimal and encode to check whether it lies in the 0 to 5 hex range  !
+				new_data=encrypt(rgb_to_hex(i[0],i[1],i[2]),binary[digit])
+				#if it lies there we get a non None value!
+				if new_data is None:
+					enc_data.append(new_data)
+				else:
+					r,g,b=hex_to_rgb(new_data)
+					#using the rgba format !
+					enc_data.append((r,g,b,255))
+					digit+=1
+			else:
+				enc_data.append(i)
+		image.putdata(enc_data)
+		image.save(out+".png","PNG")
+		print('Completed !')
+	return "Invalid Format !!"
 def extract(filename):
-	pass
+	image 
