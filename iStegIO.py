961516b280f7bfb3d66938d605eec325023036e0
Sharath Sunil <alphatrinity17@gmail.com>
2021-02-18 20:20:29 +0530

    Error Rectified at the decode section noe have to add the password

--- iStegIO.py
+++ iStegIO.py
@@ -20,17 +20,15 @@ params:hex code for color !
 return: rgb tuple
 '''
 def hex_to_rgb(hexcode):
-	#using the getcolor method we got the rgb tuple !
-	return ImageColor.getcolor(hexcode,"RGB")
+	
+	return ImageColor.getcolor(hexcode,"RGB")#using the getcolor method we got the rgb tuple !
 '''
 We first convert the string message into hexadecimal and then into binary 
 params: string message
 return: binary representation of the message
 '''
-def string_to_binary(string):
-	#the output will be 0b01100100010010
-	#we have to translate the binary to 01100100010010 
-	binary=bin(int(binascii.hexlify(string.encode()),16)).replace('0b','')
+def string_to_binary(string):	
+	binary=bin(int(binascii.hexlify(string.encode()),16)).replace('0b','')#we have to translate the binary to 01100100010010 
 	return binary
 '''
 The below funcition will convert the binary format into corresponding byte strem message 
@@ -41,7 +39,7 @@ def binary_to_string(binary):
 	# we first convert the binary to hex and then into string !
 	#each hex bit is translated into int base 2 and then to string
 	#we get the byte stream of the message !
-	message= binascii.unhexlify('%x' % (int('0b'+binary,2)))
+	message= binascii.unhexlify('%x' % (int('0b'+ binary ,2))).decode()
 	return message
 '''	
 If end of the hex code is ether 0 or 1, then return the last part of the hexcode
@@ -49,11 +47,11 @@ params: hexcode
 return: last hex
 '''
 def decrypt(hexcode):
-	if hexcode[-1] in [0,1]:
+	if hexcode[-1] in ['0','1']:
 		return hexcode[-1]
 	else:return None
 def encrypt(hexcode,digit):
-	if hexcode[-1] in ['0','1','2','3','4','5']:
+	if hexcode[-1] in ('0','1','2','3','4','5'):
 		hexcode=hexcode[:-1]+digit
 		return hexcode
 	else:return None
@@ -62,17 +60,17 @@ We'll use all the helper function above to hide the message string within the ph
 '''
 def hide(filename,message,out):
 	image =Image.open(filename)
-	#convert the message to binary and append 15 ones and one zero !
-	binary=string_to_binary(message)+'1111111111111110'
+	
+	binary=string_to_binary(message)+'1111111111111110'#convert the message to binary and append 15 ones and one zero !
 	if image.mode in ('RGBA'):
 		image =image.convert('RGBA')
-		#getting the data from the image !
-		img_data=image.getdata()
-		#the text and the image will appended here !
-		enc_data=[]
-		#the current bin digit we are working on !
+		
+		img_data=image.getdata()#getting the data from the image !
+		
+		enc_data=[]#the text and the image will appended here !
+		
 		digit = 0
-		for i in img_data:
+		for i in img_data:#the current bin digit we are working on !
 			if (digit < len(binary)):
 				#reference made to the rgb_to_hex function to convert the rgb tuple to the hexadecimal and encode to check whether it lies in the 0 to 5 hex range  !
 				new_data=encrypt(rgb_to_hex(i[0],i[1],i[2]),binary[digit])
@@ -81,8 +79,7 @@ def hide(filename,message,out):
 					enc_data.append(i)
 				else:
 					r,g,b=hex_to_rgb(new_data)
-					#using the rgba format !
-					enc_data.append((r,g,b,255))
+					enc_data.append((r,g,b,255))#using the rgba format !
 					digit+=1
 			else:
 				enc_data.append(i)
@@ -120,17 +117,24 @@ if __name__=='__main__':
 	print(heading)
 	print('VERSION 1.0')
 	print(*65*('-'))
+	password_flag='$PASSWORD$->'#the password is set to null by default and the we can set it to our choice !(The weird string is the flag !)
+	password=''
 	while True:
 		choice = int(input('1)Encrypt\n2)Decrypt\n3)Exit'))
 		if choice == 1:
 			image = f.askopenfilename()
-			mess=input('Enter the message !')
-			out=input('Output File name')
-			hide(image,mess,out)
+			message=input('Enter the message !')
+                        password =input('Enter the password : ')
+                        message+=password_flag+password
+			output=input('Output File name')
+			hide(image,message,output)
 		elif choice == 2:
 			image =f.askopenfilename()
-			extract(image)
-#			print(binary_to_string('10101010101010'))
+                        password=input('Enter the password : ')
+                        data=extract(image)
+                        image_password=data[:data.rindex(password_flag)]
+			print(image_password)
+
 		elif choice == 3:
 			print('Exiting.........')
 			break
